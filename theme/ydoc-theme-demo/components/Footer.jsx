7ad5df953199fdb12ff22d22e39883f124d4d1d0
yong.teng <tengyong@buession.com>
2022-04-08 22:00:30 +0800

    添加统计代码

--- theme/ydoc-theme-demo/components/Footer.jsx
+++ theme/ydoc-theme-demo/components/Footer.jsx
@@ -28,4 +28,13 @@
 				<p>Build by <a className="href" href="https://ydoc.ymfe.org/">ydoc</a></p>
 			</div>
 	</div>
+	<script type="text/javascript">
+		var _hmt = _hmt || [];
+		(function() {
+		  var hm = document.createElement("script");
+		  hm.src = "https://hm.baidu.com/hm.js?ce98b773508a8563e2eef8c8573da346";
+		  var s = document.getElementsByTagName("script")[0]; 
+		  s.parentNode.insertBefore(hm, s);
+		})();
+	</script>
 </footer>
