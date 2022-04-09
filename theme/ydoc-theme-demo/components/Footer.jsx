e2c2196885fb525605dfab3ca6d401e6b382849d
yong.teng <tengyong@buession.com>
2022-04-09 10:35:01 +0800

    添加统计代码

--- theme/ydoc-theme-demo/components/Footer.jsx
+++ theme/ydoc-theme-demo/components/Footer.jsx
@@ -29,11 +29,11 @@
 			</div>
 	</div>
 	{
-		var _hmt = _hmt || [];
+		const _hmt = _hmt || [];
 		(function() {
-		  var hm = document.createElement("script");
-		  hm.src = "https://hm.baidu.com/hm.js?ce98b773508a8563e2eef8c8573da346";
-		  var s = document.getElementsByTagName("script")[0]; 
+		  const hm = document.createElement('script');
+		  hm.src = 'https://hm.baidu.com/hm.js?ce98b773508a8563e2eef8c8573da346';
+		  const s = document.getElementsByTagName('script')[0]; 
 		  s.parentNode.insertBefore(hm, s);
 		})();
 	}
