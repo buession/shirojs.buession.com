e9be6160badeb58ce67027ca8556c6a52bb803b7
yong.teng <tengyong@buession.com>
2022-04-09 10:55:06 +0800

    添加统计代码

---
+++ theme/ydoc-theme-demo/scripts/stat.js
@@ -0,0 +1,6 @@
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?ce98b773508a8563e2eef8c8573da346";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
