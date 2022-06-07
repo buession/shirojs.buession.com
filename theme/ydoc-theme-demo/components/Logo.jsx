4d1c2da2d8ba600c39849816cb05502d471d83ac
yong.teng <yong.teng@liangvi.com>
2022-06-07 13:30:17 +0800

    调整 logo

--- theme/ydoc-theme-demo/components/Logo.jsx
+++ theme/ydoc-theme-demo/components/Logo.jsx
@@ -1,8 +1,5 @@
 <a href={relePath(props.distPath, "index.html")} target="_self">
   {
-    props.nav.logo ? <img className="logo" width="36" src={relePath(props.distPath, props.nav.logo)} /> : null
-  }
-  {
-    props.nav.title ? <h6 className="name">{props.nav.title}</h6> : null
+    props.nav.logo ? <img className="logo" width="70" src={relePath(props.distPath, props.nav.logo)} /> : null
   }
 </a>
